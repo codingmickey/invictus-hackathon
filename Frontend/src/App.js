27f534d5210a36b505dee9d4278ee498dd0af587
Varun Vekaria <varunvekaria@gmail.com>
2021-12-20 14:28:17 +0530

    Added Card CreateReq AcceptBid

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'; 
+import Card from './Card.js';
+import CreateReq from './CreateReq.js';
+import Bid from './Bid.js';
+import Acceptbid from './Acceptbid.js';
 function App() {
+  
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Card />
+    <CreateReq />
+     <Bid />
+   
+    </div>
     </div>
   );
 }
